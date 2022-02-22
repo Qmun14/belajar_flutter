699414d415d3b3de7c7119b5abec945e09fe31aa
Qmun14 <mamunramdhan@gmail.com>
2022-02-22 15:15:49 +0700

    Menyelesaikan halaman Bio Sosmed

--- lib/page/implementation/bio_sosmed_page.dart
+++ lib/page/implementation/bio_sosmed_page.dart
@@ -1,3 +1,4 @@
+import 'package:belajar_flutter/model/animal.dart';
 import 'package:flutter/material.dart';
 
 class BioSosmedPage extends StatelessWidget {
@@ -49,18 +50,121 @@ class BioSosmedPage extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const SizedBox(height: 16),
+                        const SizedBox(height: 10),
                         const Text(
                           'M Ramdhan',
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
                           ),
-                        )
+                        ),
+                        const SizedBox(height: 16),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 50),
+                          child: Text(
+                            "Hi.. I'm a Mobile Flutter Developer, my specialized on Android Development.\n Beside work as Mobile Developer I also worked as Full Stack Web developer ",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: const Text('Following'),
+                              style: ButtonStyle(
+                                shape: MaterialStateProperty.resolveWith(
+                                    (states) => RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                        )),
+                                backgroundColor:
+                                    MaterialStateProperty.resolveWith(
+                                        (states) =>
+                                            Color.fromARGB(255, 165, 72, 219)),
+                              ),
+                            ),
+                            const SizedBox(width: 16),
+                            OutlinedButton(
+                              onPressed: () {},
+                              child: const Text('Message'),
+                              style: ButtonStyle(
+                                side: MaterialStateProperty.resolveWith(
+                                    (states) =>
+                                        const BorderSide(color: Colors.blue)),
+                                shape: MaterialStateProperty.resolveWith(
+                                    (states) => RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30))),
+                              ),
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            builtItem('Posts', '1.5k'),
+                            Container(
+                                height: 40, width: 3, color: Colors.purple),
+                            builtItem('Followers', '49.8k'),
+                            Container(
+                                height: 40, width: 3, color: Colors.purple),
+                            builtItem('Following', '2.5k'),
+                          ],
+                        ),
                       ],
                     ),
                   ],
-                )
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      Animal animal = listAnimal[index];
+                      return Container(
+                        width: 130,
+                        margin: EdgeInsets.fromLTRB(
+                          index == 0 ? 16 : 8,
+                          0,
+                          index == 3 ? 16 : 8,
+                          0,
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
+                            animal.image,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 8),
+                TextButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      SizedBox(width: 8),
+                      Text('See All'),
+                      SizedBox(width: 8),
+                      Icon(Icons.navigate_next)
+                    ],
+                  ),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.resolveWith((states) =>
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+                const SizedBox(height: 16),
               ],
             ),
           ),
@@ -79,6 +183,23 @@ class BioSosmedPage extends StatelessWidget {
               ],
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget builtItem(String title, String data) {
+    return Container(
+      width: 120,
+      child: Column(
+        children: [
+          Text(data,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16)),
+          const SizedBox(height: 10),
+          Text(title, style: const TextStyle(fontSize: 14))
         ],
       ),
     );

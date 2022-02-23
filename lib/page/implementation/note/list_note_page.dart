7e87c4f6fb66620659faf34e260edb09cb68df46
Qmun14 <mamunramdhan@gmail.com>
2022-02-23 15:59:58 +0700

    Mempelajari Materi CRUD awal SQfLite dengan membuat DB, Model dan UI nya

---
+++ lib/page/implementation/note/list_note_page.dart
@@ -0,0 +1,50 @@
+import 'package:belajar_flutter/db/db_note.dart';
+import 'package:belajar_flutter/model/note.dart';
+import 'package:flutter/material.dart';
+
+class ListNotePage extends StatefulWidget {
+  @override
+  _ListNotePageState createState() => _ListNotePageState();
+}
+
+class _ListNotePageState extends State<ListNotePage> {
+  List<Note> _listNote = [];
+
+  void getListNote() async {
+    _listNote.clear();
+    _listNote = await DBNote().getAllNote();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getListNote();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('CRUD Note'), titleSpacing: 0),
+      body: _listNote.isNotEmpty
+          ? ListView.builder(
+              itemCount: _listNote.length,
+              itemBuilder: (context, index) {
+                Note note = _listNote[index];
+                return ListTile(
+                  title: Text(note.title ?? ''),
+                  trailing: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.delete)),
+                );
+              },
+            )
+          : const Center(
+              child: Text('Empty'),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/page/menu_page.dart
+++ lib/page/menu_page.dart
@@ -2,6 +2,7 @@ import 'package:belajar_flutter/page/flutter_network_page.dart';
 import 'package:belajar_flutter/page/implementation/basket_counter_page.dart';
 import 'package:belajar_flutter/page/implementation/bio_sosmed_page.dart';
 import 'package:belajar_flutter/page/implementation/calculator_page.dart';
+import 'package:belajar_flutter/page/implementation/note/list_note_page.dart';
 import 'package:belajar_flutter/page/implementation/random_dice_page.dart';
 import 'package:belajar_flutter/page/implementation/webview_page.dart';
 import 'package:belajar_flutter/page/interaction/action_page.dart';
@@ -380,6 +381,17 @@ class MenuPage extends StatelessWidget {
                   );
                 },
                 child: const Text('Bio Sosmed Page'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ListNotePage(),
+                    ),
+                  );
+                },
+                child: const Text('List Note Page'),
               ),
             ],
           ),
